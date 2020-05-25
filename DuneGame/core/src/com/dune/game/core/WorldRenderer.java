5abc02d25c3ef115a651f8225c455746b1255fed
andrey_kazakov <aakazakovv@yandex.ru>
2020-05-25 22:05:39 +0300

    fix projectiles rendering

--- DuneGame/core/src/com/dune/game/core/WorldRenderer.java
+++ DuneGame/core/src/com/dune/game/core/WorldRenderer.java
@@ -19,7 +19,7 @@ public class WorldRenderer {
     batch.begin();
     gc.getMap().render(batch);
     gc.getTank().render(batch);
-    gc.getProjectilesController();
+    gc.getProjectilesController().render(batch);
     batch.end();
   }
 }
