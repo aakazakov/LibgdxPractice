321ee254778e5ac7de9de6348b6a10d237d6b4c4
andrey_kazakov <aakazakovv@yandex.ru>
2020-05-25 22:06:25 +0300

    refactorrro)

--- DuneGame/core/src/com/dune/game/core/Tank.java
+++ DuneGame/core/src/com/dune/game/core/Tank.java
@@ -4,46 +4,66 @@ import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.math.*;
 
-public class Tank {
-  private Vector2 position;
+public class Tank extends GameObject {
   private TextureRegion[] textures;
+  private Vector2 destination;
   private float angle;
   private float speed;
+  private float rotationSpeed;
   private float moveTimer;
   private float timePerFrame;
   
-  private Projectile bullet;
-  
-  public Tank(TextureAtlas atlas, float x, float y) {
-    this.position = new Vector2(x, y);
-    this.textures = new TextureRegion(atlas.findRegion("tankanim")).split(64, 64)[0];
-    this.speed = 140.0f;
+  public Tank(GameController gc, float x, float y) {
+    super(gc);
+    this.position.set(x, y);
+    this.destination = new Vector2(position);
+    this.textures = Assets.getInstance().getAtlas().findRegion("tankanim").split(64, 64)[0];
+    this.speed = 120.0f;
     this.timePerFrame = 0.08f;
-    this.bullet = new Projectile(atlas);
+    this.rotationSpeed = 90.0f;
   }
   
-  private int getCurrentFrameIndex() {
-    return (int) (moveTimer / timePerFrame) % textures.length;
-  }
-
   public void update(float dt) {
-    if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-      angle += 180.0f * dt;
+    if (Gdx.input.justTouched()) {
+      destination.set(Gdx.input.getX(), 720 - Gdx.input.getY());
     }
-    if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-      angle -= 180.0f * dt;
-    }
-    if (Gdx.input.isKeyPressed(Input.Keys.UP)) {      
-      position.add(speed * MathUtils.cosDeg(angle) * dt, speed * MathUtils.sinDeg(angle) * dt);
-      moveTimer += dt;      
-    } else {
-      moveTimer = 0;
+    if (position.dst(destination) > 3.0f) {
+      float angleTo = tmp.set(destination).sub(position).angle();
+      if (angle > angleTo) {
+        if (Math.abs(angle - angleTo) <= 180.0f) {
+          angle -= rotationSpeed * dt;
+        } else {
+          angle += rotationSpeed * dt;
+        }
+      } else {
+        if (Math.abs(angle - angleTo) <= 180.0f) {
+          angle += rotationSpeed * dt;
+        } else {
+          angle -= rotationSpeed * dt;
+        }
+      }
+      if (angle < 0.0f) {
+        angle += 360.0f;
+      }
+      if (angle > 360.0f) {
+        angle -= 360.0f;
+      }
+      moveTimer += dt;
+      tmp.set(speed, 0).rotate(angle);
+      position.mulAdd(tmp, dt);
+      if (position.dst(destination) < 120.0f && Math.abs(angleTo - angle) > 10) {
+        position.mulAdd(tmp, -dt);
+      }
     }
     if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-      bullet.setLaunchPosition(position, angle);        
+      fire();
     }
-    bullet.update(dt);
     checkBounds();
+  }
+  
+  public void fire() {
+    tmp.set(position).add(32 * MathUtils.cosDeg(angle), 32 * MathUtils.sinDeg(angle));
+    gc.getProjectilesController().setup(tmp, angle);
   }
   
   private void checkBounds() {
@@ -55,6 +75,10 @@ public class Tank {
 
   public void render(SpriteBatch batch) {
     batch.draw(textures[getCurrentFrameIndex()], position.x - 40, position.y - 40, 40, 40, 80, 80, 1, 1, angle);
-    bullet.render(batch);
   }
+  
+  private int getCurrentFrameIndex() {
+    return (int) (moveTimer / timePerFrame) % textures.length;
+  }
+  
 }
