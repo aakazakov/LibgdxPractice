f16535e418e0e6ad9e29ceeffc843e4a137931f4
andrey_kazakov <aakazakovv@yandex.ru>
2020-05-26 14:44:39 +0300

    add resources

--- DuneGame/core/src/com/dune/game/core/BattleMap.java
+++ DuneGame/core/src/com/dune/game/core/BattleMap.java
@@ -1,12 +1,20 @@
 package com.dune.game.core;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.math.*;
 
 public class BattleMap {
-  TextureRegion grassTexture;
+  private TextureRegion grassTexture;
+  
+  private Texture resource;
+  private boolean[][] resourceMap;
   
   public BattleMap() {
     this.grassTexture = Assets.getInstance().getAtlas().findRegion("grass");
+    
+    this.resource = new Texture("smile.png");
+    this.resourceMap = getResourceMap();
   }
   
   public void render(SpriteBatch batch) {
@@ -15,5 +23,28 @@ public class BattleMap {
         batch.draw(grassTexture, i * 80, j * 80);
       }
     }
-  }  
+    
+    for (int i = 0; i < 16; i++) {
+      for (int j = 0; j < 9; j++) {
+        if (resourceMap[i][j]) {
+          batch.draw(resource, i * 80, j * 80);
+        }
+      }
+    }
+  }
+  
+  private boolean[][] getResourceMap() {
+    boolean[][] result = new boolean[16][9];
+    for (int i = 0; i < 16; i++) {
+      for (int j = 0; j < 9; j++) {
+        if (getRandom() == 10) result[i][j] = true;
+      }
+    }
+    return result;
+  }
+  
+  private int getRandom() {
+    return MathUtils.random(10);
+  }
+  
 }
