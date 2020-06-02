5f3fa3793cc34edfe24b1ad68c7eeb42ec225460
andrey_kazakov <aakazakovv@yandex.ru>
2020-06-02 09:59:55 +0300

    add multy selection

--- DuneGame/core/src/com/dune/game/core/GameController.java
+++ DuneGame/core/src/com/dune/game/core/GameController.java
@@ -3,6 +3,7 @@ package com.dune.game.core;
 import java.util.*;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.Input.Buttons;
 import com.badlogic.gdx.math.*;
 import com.dune.game.core.Tank.Owner;
 
@@ -12,6 +13,7 @@ public class GameController {
   private TanksController tanksController;
   private Set<Tank> selectedUnits;
   private Vector2 tmp;
+  private Vector2 selectionStart;
 
   public GameController() {
     Assets.getInstance().loadAssets();
@@ -20,32 +22,84 @@ public class GameController {
     this.tanksController = new TanksController(this);
     this.selectedUnits = new HashSet<>();
     this.tmp = new Vector2();
+    this.selectionStart = new Vector2();
     for (int i = 0; i < 5; i++) {
       tanksController.setup(MathUtils.random(80, 1200), MathUtils.random(80, 640), Owner.PLAYER);
     }
+    prepareInput();
+  }
+  
+  private void prepareInput() {
+    InputProcessor ip = new InputAdapter() {
+      @Override
+      public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        if (button == Buttons.LEFT) {
+          selectionStart.set(screenX, 720 - screenY);
+        }
+        return true;
+      }
+      
+      @Override
+      public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        if (button == Buttons.LEFT) {
+          tmp.set(Gdx.input.getX(), 720 - Gdx.input.getY());
+          if (tmp.x < selectionStart.x) {
+            float t = tmp.x;
+            tmp.x = selectionStart.x;
+            selectionStart.x = t;
+          }
+          if (tmp.y > selectionStart.y) {
+            float t = tmp.y;
+            tmp.y = selectionStart.y;
+            selectionStart.y = t;
+          }
+          selectedUnits.clear();
+          if (Math.abs(tmp.x - selectionStart.x) > 20 && Math.abs(tmp.y - selectionStart.y) > 20) {
+            for (int i = 0; i < tanksController.activeSize(); i++) {
+              Tank t = tanksController.getActiveList().get(i);
+              if (t.getPosition().x > selectionStart.x
+                  && t.getPosition().x < tmp.x
+                  && t.getPosition().y > tmp.y
+                  && t.getPosition().y < selectionStart.y) {
+                selectedUnits.add(t);
+              }
+            }
+          } else {
+            for (int i = 0; i < tanksController.activeSize(); i++) {
+              Tank t = tanksController.getActiveList().get(i);
+              if (t.getPosition().dst(tmp) < 30.0f) {
+                selectedUnits.add(t);
+              }
+            }
+          }
+        }
+        return true;
+      }
+    };
+    Gdx.input.setInputProcessor(ip);
   }
   
   public void update(float dt) {
     projectilesController.update(dt);
     map.update(dt);
     tanksController.update(dt);
-    checkSelection();
+//    checkSelection();
     checkCollisions();
   }
   
-  private void checkSelection() {
-    if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-      selectedUnits.clear();
-      tmp.set(Gdx.input.getX(), 720 - Gdx.input.getY());
-      Tank t;
-      for (int i = 0; i < tanksController.activeSize(); i++) {
-        t = tanksController.getActiveList().get(i);
-        if (t.getPosition().dst(tmp) < 30.0f) {
-          selectedUnits.add(t);
-        }
-      }
-    }
-  }
+//  private void checkSelection() {
+//    if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
+//      selectedUnits.clear();
+//      tmp.set(Gdx.input.getX(), 720 - Gdx.input.getY());
+//      Tank t;
+//      for (int i = 0; i < tanksController.activeSize(); i++) {
+//        t = tanksController.getActiveList().get(i);
+//        if (t.getPosition().dst(tmp) < 30.0f) {
+//          selectedUnits.add(t);
+//        }
+//      }
+//    }
+//  }
   
   public boolean isTankSelected(Tank t) {
     return selectedUnits.contains(t);
