fc516b0f3504969539aa1303fd6575c969746aee
andrey_kazakov <aakazakovv@yandex.ru>
2020-06-08 17:20:21 +0300

    fix progressbar position

--- DuneGame/core/src/com/dune/game/core/units/Harvester.java
+++ DuneGame/core/src/com/dune/game/core/units/Harvester.java
@@ -49,9 +49,9 @@ public class Harvester extends AbstractUnit {
     super.renderGui(batch);
     if (weapon.getUsageTimePercentage() > 0.0f) {
       batch.setColor(0.2f, 0.2f, 0.0f, 1.0f);
-      batch.draw(progressbarTexture, position.x - 20, position.y + 30, 64, 12);
+      batch.draw(progressbarTexture, position.x - 32, position.y + 22, 64, 8);
       batch.setColor(1.0f, 1.0f, 0.0f, 1.0f);
-      batch.draw(progressbarTexture, position.x - 18, position.y + 32, 60 * weapon.getUsageTimePercentage(), 8);
+      batch.draw(progressbarTexture, position.x - 30, position.y + 24, 60 * weapon.getUsageTimePercentage(), 4);
       batch.setColor(1.0f, 1.0f, 1.0f, 1.0f);
     }
   }
